1362acd2c12c87b722f6975a91b42b10c515f400
folomeev1980 <a.a.folomeev80@gmail.com>
2021-05-30 16:58:02 +0300

    radiobot may2021 v2

--- config.py
+++ config.py
@@ -27,7 +27,7 @@ def convert_low32(filename):
         "outputformat": "mp3",
         "input": "upload",
         "converteroptions": {
-            "audio_bitrate": 48,
+            "audio_bitrate": 32,
             "audio_frequency": "44100",
             "audio_codec": "MP3",
             "audio_qscale": -1,
