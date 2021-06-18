4ff17347c91a797001abe28c766713906373d10f
folomeev1980 <40419806+folomeev1980@users.noreply.github.com>
2021-06-19 07:59:28 +1100

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import os
 import config
 import logging
 import  time
-from pytube import YouTube as YouTube3
+from pytube3.__main__ import YouTube as YouTube3
 import re
 import time
 from telegram.ext import Updater, MessageHandler, Filters, CommandHandler
