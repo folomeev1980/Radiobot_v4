55aa2d99292d070cc4ff2479e52481747630cd86
folomeev1980 <a.a.folomeev80@gmail.com>
2021-06-20 06:10:33 +1100

    disable_converter

--- bot.py
+++ bot.py
@@ -6,6 +6,10 @@ Source: https://github.com/python-telegram-bot/python-telegram-bot/blob/master/e
 
 import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import config
+import re
+from pytube import YouTube as YouTube3
+
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -36,25 +40,60 @@ def error(update, context):
     """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
+def link(update, context):
+    try:
+        if len(re.findall(r'(https?://[^\s]+)', update.message.text)) > 0:
+
+            config.remove_files()
+            mp3_link = config.youtube_link(update.message.text)
+
+            yt = YouTube3(mp3_link)
+
+            filename = "input.webm"
+            titl = str(yt.title)[0:35]
+            audio = yt.streams.filter(only_audio=True, file_extension="webm")[0]
+            #update.message.reply_text("\n....Начало скачивания....")
+            audio.download(filename='input')
+
+            update.message.reply_text("Начало конвертации: " + config.file_size(filename))
+            config.convert_low32(filename)
+            update.message.reply_text("Конец конвертации: " + config.file_size('output.mp3'))
+            context.bot.send_chat_action(update.message.chat.id, 'upload_audio')
+            audio = open("output.mp3", 'rb')
+            context.bot.send_audio(update.message.chat.id, audio, title=titl)
+
+
+
+
+
+        else:
+            print(update.message.chat.id)
+            update.message.reply_text(config.help)
+
+    except Exception as ex:
+        update.message.reply_text("Error <<<{}>>>>".format(str(ex)))
+
+
+def update(update, context):
+    update.message.reply_text(str(update.message))
+
 def main():
-    """Start the bot."""
-    # Create the Updater and pass it your bot's token.
-    # Make sure to set use_context=True to use the new context based callbacks
-    # Post version 12 this will no longer be necessary
+
+
     updater = Updater(TOKEN, use_context=True)
+    dispatcher = updater.dispatcher
 
-    # Get the dispatcher to register handlers
-    dp = updater.dispatcher
+    dispatcher.add_error_handler(error)
 
-    # on different commands - answer in Telegram
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help))
+    #    Commands
 
-    # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler(Filters.text, echo))
+    update_handler = CommandHandler('update', update)
+    dispatcher.add_handler(update_handler)
 
-    # log all errors
-    dp.add_error_handler(error)
+    #    Messages
+
+    link_handler = MessageHandler(Filters.text, link)
+    dispatcher.add_handler(link_handler)
 
     # Start the Bot
     if not "radiobotv4":  # pooling mode
@@ -79,4 +118,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-#view rawpythontelegrambot.py hosted with ❤ by GitHub
