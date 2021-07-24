bf9a9b7d0784aa90c92eff64b5176ac09ed34eff
folomeev1980 <a.a.folomeev80@gmail.com>
2021-07-25 00:26:06 +1100

    30.06.2021 40 bit

--- testDownload.py
+++ testDownload.py
@@ -1,10 +1,22 @@
-import pytube
+import requests
+import socket
 
-yt = pytube.YouTube("https://www.youtube.com/watch?v=1WbS2P_dxOM",
-                    proxies={"https": "https://10.104.1.9:8080"})
+def _execute_request(
+    url,
+    method=None,
+    headers=None,
+    data=None,
+    timeout=socket._GLOBAL_DEFAULT_TIMEOUT
+):
+    video_url = url[url.find('watch?v=') + 8:url.find('watch?v=') + 19]
+    base_headers = {
+        "POST": "/youtubei/v1/player HTTP/1.1",
+        "Host": "www.youtube.com",
+        "X-Goog-Api-Key": "AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8"
+    }
+    data = {"context":{"client":{"clientName":"ANDROID","clientVersion":"16.05"}},
+    "videoId":video_url}
+    return requests.post(url="https://www.youtube.com/youtubei/v1/player", headers=base_headers, json = data).text
 
-
-video = yt.streams[0]
-
-print(video)
-#video.download(filename='C://Users//folomeev//Videos//Captures//win11')
+with open("source2.html", encoding='utf-8', mode='w') as f:
+    f.write(_execute_request(url="https://www.youtube.com/watch?v=Z1wsRUTMAPI"))
