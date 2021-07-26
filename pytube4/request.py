6b851c85b9439f581183eab35f243f9aaaa8a46d
folomeev1980 <a.a.folomeev80@gmail.com>
2021-07-27 02:20:22 +1100

    27.07.2021 pytube4

--- pytube4/request.py
+++ pytube4/request.py
@@ -23,17 +23,22 @@ def _execute_request(
     data=None,
     timeout=socket._GLOBAL_DEFAULT_TIMEOUT
 ):
-    import requests
-    video_url = url[url.find('watch?v=') + 8:url.find('watch?v=') + 19]
     base_headers = {
         "POST": "/youtubei/v1/player HTTP/1.1",
         "Host": "www.youtube.com",
-        "X-Goog-Api-Key": "AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8"
-    }
-    data = {"context":{"client":{"clientName":"ANDROID","clientVersion":"16.05"}},
-    "videoId":video_url}
-    return requests.post(url="https://www.youtube.com/youtubei/v1/player", headers=base_headers, json = data).text
-
+        "X-Goog-Api-Key": "AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8"}
+    if headers:
+        base_headers.update(headers)
+    if data:
+        # encode data for request
+        if not isinstance(data, bytes):
+            data = bytes(json.dumps(data), encoding="utf-8")
+    
+    if url.lower().startswith("http"):
+        request = Request(url, headers=base_headers, method=method, data=data)
+    else:
+        raise ValueError("Invalid URL")
+    return urlopen(request, timeout=timeout)  # nosec
 
 
 def get(url, extra_headers=None, timeout=socket._GLOBAL_DEFAULT_TIMEOUT):
