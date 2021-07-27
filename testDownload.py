a658ee9075a25d69783c700c104a201090f30fc4
folomeev1980 <a.a.folomeev80@gmail.com>
2021-07-28 02:27:32 +1100

    27.07.2021 youtube-dl added

--- testDownload.py
+++ testDownload.py
@@ -4,12 +4,13 @@ import subprocess
 
 
 #
-bashCommand = "youtube-dl --proxy 10.104.1.9:8080 -f worstaudio https://www.youtube.com/watch?v=NiPWr00iDeQ"
-#bashCommand = "youtube-dl --proxy 10.104.1.9:8080 --title https://www.youtube.com/watch?v=V_6WlQuMplg"
+#bashCommand = "youtube-dl --proxy 10.104.1.9:8080 -f worstaudio https://www.youtube.com/watch?v=NiPWr00iDeQ"
+bashCommand = "youtube-dl -f worstaudio https://www.youtube.com/watch?v=V_6WlQuMplg"
 #
 # process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
 # output, error = process.communicate()
 
-process = subprocess.Popen(bashCommand, stdout=subprocess.PIPE, shell=True)
+#process = subprocess.Popen(bashCommand, stdout=subprocess.PIPE, shell=True)
+process = subprocess.Popen(bashCommand, stdout=subprocess.PIPE)
 output, error = process.communicate()
 print(output)
