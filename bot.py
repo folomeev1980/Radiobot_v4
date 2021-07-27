785429a2b3296eb14b3b719d7c648a58af064851
folomeev1980 <a.a.folomeev80@gmail.com>
2021-07-28 02:47:42 +1100

    27.07.2021 youtube-dl added

--- bot.py
+++ bot.py
@@ -1,4 +1,3 @@
-
 """
 Simple Bot to reply to Telegram messages taken from the python-telegram-bot examples.
 Source:
@@ -10,15 +9,15 @@ import config
 import re
 from pytube import YouTube as YouTube3
 import os
-
+import subprocess
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
 logger = logging.getLogger(__name__)
-#TOKEN = '574990729:AAHvFVDSNg-LQ5RUSaPdbiQ2pOdDA7XI5Xc'
-#TOKEN = os.environ["TOKEN"]
+# TOKEN = '574990729:AAHvFVDSNg-LQ5RUSaPdbiQ2pOdDA7XI5Xc'
+# TOKEN = os.environ["TOKEN"]
 TOKEN = None
 
 # with open("tk.txt") as f:
@@ -26,13 +25,15 @@ TOKEN = None
 #
 #
 
-#import os
+# import os
 
 TOKEN = os.environ["TOKEN"]
-app_name="radiobotv4"
+app_name = "radiobotv4"
 
 import os
+
 PORT = int(os.environ.get('PORT', 443))
+
 
 # Define a few command handlers. These usually take the two arguments update and
 # context. Error handlers also receive the raised TelegramError object in error.
@@ -40,17 +41,21 @@ def start(update, context):
     """Send a message when the command /start is issued."""
     update.message.reply_text('Hi!')
 
+
 def help(update, context):
     """Send a message when the command /help is issued."""
     update.message.reply_text('Help!')
+
 
 def echo(update, context):
     """Echo the user message."""
     update.message.reply_text(update.message.text)
 
+
 def error(update, context):
     """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, context.error)
+
 
 def link(update, context):
     try:
@@ -63,19 +68,27 @@ def link(update, context):
 
             filename = "input.webm"
             titl = str(yt.title)[0:35]
-            audio = yt.streams.filter(only_audio=True, file_extension="webm")[0]
-            #update.message.reply_text("\n....Начало скачивания....")
-            audio.download(filename='input')
+
+            # audio = yt.streams.filter(only_audio=True, file_extension="webm")[0]
+            # update.message.reply_text("\n....Начало скачивания....")
+            # audio.download(filename='input')
+
+            #
+            bashCommand = "youtube-dl -f worstaudio -o input.webm " + mp3_link
+
+            process = subprocess.Popen(bashCommand, stdout=subprocess.PIPE, shell=True)
+            output, error = process.communicate()
+            print(output)
 
             update.message.reply_text("Начало конвертации: " + config.file_size(filename))
 
-            #--------------------------------------------------------------------------------
+            # --------------------------------------------------------------------------------
             config.convert_low32(filename)
-           # update.message.reply_text("Конец конвертации: " + config.file_size('output.mp3'))
+            # update.message.reply_text("Конец конвертации: " + config.file_size('output.mp3'))
             context.bot.send_chat_action(update.message.chat.id, 'upload_audio')
             audio = open("output.mp3", 'rb')
             context.bot.send_audio(update.message.chat.id, audio, title=titl)
-            #--------------------------------------------------------------------------------
+            # --------------------------------------------------------------------------------
 
             # config.convert_low32(filename)
             # update.message.reply_text("Конец конвертации: " + config.file_size('output.mp3'))
@@ -97,9 +110,8 @@ def link(update, context):
 def update(update, context):
     update.message.reply_text(str(update.message))
 
+
 def main():
-
-
     updater = Updater(TOKEN, use_context=True)
     dispatcher = updater.dispatcher
 
@@ -124,16 +136,18 @@ def main():
         updater.idle()
 
     else:  # webhook mode
-        print("Running bot in webhook mode. Make sure that this url is correct: https://{}.herokuapp.com/".format(app_name))
+        print("Running bot in webhook mode. Make sure that this url is correct: https://{}.herokuapp.com/".format(
+            app_name))
         updater.start_webhook(
             listen="0.0.0.0",
             port=PORT,
             url_path=TOKEN,
-            webhook_url="https://{}.herokuapp.com/{}".format(app_name,TOKEN)
+            webhook_url="https://{}.herokuapp.com/{}".format(app_name, TOKEN)
         )
 
         #    updater.bot.set_webhook(f"https://{HEROKU_APP_NAME}.herokuapp.com/{TELEGRAM_TOKEN}")
         updater.idle()
+
 
 if __name__ == '__main__':
     main()
--- testDownload.py
+++ testDownload.py
@@ -4,13 +4,13 @@ import subprocess
 
 
 #
-#bashCommand = "youtube-dl --proxy 10.104.1.9:8080 -f worstaudio https://www.youtube.com/watch?v=NiPWr00iDeQ"
-bashCommand = "youtube-dl -f worstaudio https://www.youtube.com/watch?v=V_6WlQuMplg"
+bashCommand = "youtube-dl --proxy 10.104.1.9:8080 -f worstaudio -o input.webm https://www.youtube.com/watch?v=I1FZtftKzPo"
+#bashCommand = "youtube-dl -f worstaudio https://www.youtube.com/watch?v=V_6WlQuMplg"
 #
 # process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
 # output, error = process.communicate()
 
 #process = subprocess.Popen(bashCommand, stdout=subprocess.PIPE, shell=True)
-process = subprocess.Popen(bashCommand, stdout=subprocess.PIPE)
+process = subprocess.Popen(bashCommand, stdout=subprocess.PIPE, shell=True)
 output, error = process.communicate()
 print(output)
